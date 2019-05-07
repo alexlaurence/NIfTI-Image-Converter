99caf8b77861f214bd9b960151b272f2d431fbd8
Alexander Laurence <39573687+alexlaurence@users.noreply.github.com>
2019-05-07 20:43:20 +0100

    Update nii2png.py

--- nii2png.py
+++ nii2png.py
@@ -19,40 +19,76 @@ od = input('Finally, please enter the name for your output folder: ')
 # set fn as your 4d nifti file
 image_array = nibabel.load(fn).get_data()
 
-# set 4d array dimension values
-nx, ny, nz, nw = image_array.shape
+# if 4D image inputted
+if len(image_array) == 4:
+        # set 4d array dimension values
+        nx, ny, nz, nw = image_array.shape
 
-# set destination folder
-dst = wd + "/" + od + "_" + fn[:-4]
-if not os.path.exists(dst):
-        os.makedirs(dst)
-        print("Created ouput directory: " + dst)
+        # set destination folder
+        dst = wd + "/" + od + "_" + fn[:-4]
+        if not os.path.exists(dst):
+                os.makedirs(dst)
+                print("Created ouput directory: " + dst)
 
-print("Converting NIfTI to png...")
+        print("Converting NIfTI to png...")
 
-volumes = image_array.shape[3]
-slices = image_array.shape[2]
+        volumes = image_array.shape[3]
+        slices = image_array.shape[2]
 
-volume_counter = 0
-image_counter = 0
+        volume_counter = 0
+        image_counter = 0
 
-# iterate through volumes
-for j in range(0, volumes):
-  slice_counter = 0
-  # iterate through slices
-  for i in range(0, slices):
-    # set nifti as a numpy array
-    data = numpy.rot90(image_array[:, :, i, j])
-    #alternate slices and save as png
-    if (slice_counter % 1) == 0:
-      image_name = fn[:-4] + "_t" "{:0>3}".format(str(j)) + "_z" + "{:0>3}".format(str(i))+ ".png"
-      scipy.misc.imsave(image_name, data)
-      
-      #move images to folder
-      src = wd + image_name
-      shutil.move(src, dst)
-      slice_counter += 1
-      image_counter += 1
-  volume_counter += 1
+        # iterate through volumes
+        for j in range(0, volumes):
+          slice_counter = 0
+          # iterate through slices
+          for i in range(0, slices):
+            # set nifti as a numpy array
+            data = numpy.rot90(image_array[:, :, i, j])
+            #alternate slices and save as png
+            if (slice_counter % 1) == 0:
+              image_name = fn[:-4] + "_t" "{:0>3}".format(str(j)) + "_z" + "{:0>3}".format(str(i))+ ".png"
+              scipy.misc.imsave(image_name, data)
 
-print("Finished converting images") 
+              #move images to folder
+              src = wd + image_name
+              shutil.move(src, dst)
+              slice_counter += 1
+              image_counter += 1
+          volume_counter += 1
+
+        print("Finished converting images") 
+
+# else if 3D image inputted
+elif len(image_array) == 3:
+        # set 4d array dimension values
+        nx, ny, nz = image_array.shape
+
+        # set destination folder
+        dst = wd + "/" + od + "_" + fn[:-4]
+        if not os.path.exists(dst):
+                os.makedirs(dst)
+                print("Created ouput directory: " + dst)
+
+        print("Converting NIfTI to png...")
+
+        slices = image_array.shape[2]
+
+        image_counter = 0
+        slice_counter = 0
+        # iterate through slices
+        for i in range(0, slices):
+                # set nifti as a numpy array
+                data = numpy.rot90(image_array[:, :, i])
+                #alternate slices and save as png
+                if (slice_counter % 1) == 0:
+                        image_name = fn[:-4] + "_t" "{:0>3}".format(str(j)) + "_z" + "{:0>3}".format(str(i))+ ".png"
+                        scipy.misc.imsave(image_name, data)
+
+                        #move images to folder
+                        src = wd + image_name
+                        shutil.move(src, dst)
+                        slice_counter += 1
+                        image_counter += 1
+
+        print("Finished converting images") 
